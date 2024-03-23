d6daf57a12f95df9949b3613022ef31988a2505c
jiyong <parkjiy96@naver.com>
2024-03-23 14:03:53 +0900

    ♻️ refactor : 프로필 드랍다운 디자인 수정

--- src/components/molecules/profileDropdown/index.tsx
+++ src/components/molecules/profileDropdown/index.tsx
@@ -4,10 +4,14 @@ import Link from 'next/link';
 import styles from './profileDropdown.module.scss';
 import { useRouter } from 'next/router';
 import { ProfileDownProps } from '@/@types/type';
+import { useContext } from 'react';
+import { userContext } from '@/pages/_app';
+import Image from 'next/image';
 
 const cn = classNames.bind(styles);
 
 const ProfileDown = ({ onBlur }: ProfileDownProps) => {
+  const { userInfo } = useContext(userContext);
   const router = useRouter();
   const handleLogout = () => {
     sessionStorage.removeItem('accessToken');
@@ -17,13 +21,23 @@ const ProfileDown = ({ onBlur }: ProfileDownProps) => {
 
   return (
     <div className={cn('nicknameMenu')} onBlur={onBlur}>
-      <button className={cn('menuItem')}>
-        <Link href="/myPage">마이페이지</Link>
-      </button>
-      <hr />
-      <button className={cn('menuItem', 'logout')} onClick={handleLogout}>
-        로그아웃
-      </button>
+      <Image
+        src={userInfo.profileImageUrl}
+        alt="userImage"
+        width={80}
+        height={80}
+        className={cn('profileImage')}
+      />
+      <span>{userInfo.nickname}</span>
+      <span>{userInfo.email}</span>
+      <div className={cn("btnContainer")}>
+        <button className={cn('menuItem')}>
+          <Link href="/myPage">MyPage</Link>
+        </button>
+        <button className={cn('menuItem', 'logout')} onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
